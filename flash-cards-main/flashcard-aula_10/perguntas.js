9620a2166d9cbf347fd2e5ae54cfa6ad07d78a22
diogoeisabeli <166416900+diogoeisabeli@users.noreply.github.com>
2024-10-09 08:03:45 -0300

    Add files via upload

---
+++ flash-cards-main/flashcard-aula_10/perguntas.js
@@ -0,0 +1,72 @@
+criaCartao(
+    'Matemática',
+    O que é um número primo?
+Um número primo é um número natural maior que 1 que só pode ser dividido por 1 e por ele mesmo.
+)
+
+criaCartao(
+    'Matemática',
+    Qual é a fórmula para calcular a área de um triângulo?
+A área de um triângulo é calculada pela fórmula A=2b×h​, onde b é a base e h é a altura.
+)
+
+criaCartao(
+    'Matemática',
+    O que representa o π (pi)?
+    O π (pi) é a razão entre a circunferência de um círculo e seu diâmetro, aproximadamente 3,14.
+)
+
+criaCartao(
+    'Matemática',
+    Como se calcula a média de um conjunto de números?
+ A média é calculada somando todos os números e dividindo pelo total de números no conjunto.
+)
+
+criaCartao(
+    'Matemática',
+    O que é um ângulo reto?
+ Um ângulo reto é um ângulo que mede exatamente 90 graus.
+)
+
+criaCartao(
+    'Matemática',
+    Qual é a fórmula para a área de um círculo?
+ A área de um círculo é dada por A=π×r2, onde r é o raio.
+)
+
+criaCartao(
+    'Matemática',
+    O que são números inteiros?
+Números inteiros são todos os números positivos, negativos e o zero, sem frações ou decimais.
+)
+
+criaCartao(
+    'Matemática',
+    Como se calcula a soma de um ângulo interno de um polígono?
+    A soma dos ângulos internos de um polígono é dada pela fórmula (n−2)×180, onde n é o número de lados.
+)
+
+criaCartao(
+    'Matemática',
+    O que é um número irracional?
+    Um número irracional é um número que não pode ser expresso como uma fração de dois inteiros, como √2 ou π.
+)
+
+criaCartao(
+    'Matemática',
+    O que é uma progressão aritmética?
+ Uma progressão aritmética (PA) é uma sequência de números em que a diferença entre termos consecutivos é constante.
+)
+
+criaCartao(
+    'Matemática',
+    Como se calcula o volume de um cubo?
+    O volume de um cubo é calculado pela fórmula V=a3, onde a é o comprimento da aresta.
+)
+
+criaCartao(
+    'Matemática',
+    • O que é um gráfico de função?
+    Um gráfico de função é uma representação visual de uma função matemática, mostrando a relação entre as variáveis em um plano cartesiano.
+   
+)
